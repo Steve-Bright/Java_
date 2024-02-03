2fc6a34c19b0a4360406106b31f23a56bcbfa00d
Steve Bright <swemhtet.net@gmail.com>
2024-02-03 16:34:59 +0630

    created repo for java

---
+++ src/edp/assignment_swam/htet/NegativeNumException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edp.assignment_swam.htet;
+
+/**
+ *
+ * @author May Thaik
+ */
+public class NegativeNumException extends Exception {
+
+    public NegativeNumException(String error_message) {
+        super(error_message);
+    }
+    
+}
